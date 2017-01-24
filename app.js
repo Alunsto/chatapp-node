61ea0c19f21d139f1d635185c5d733952b1b19a2
Alunsto <alunsto99@gmail.com>
2017-01-23 19:53:32 -0500

    Usability overhaul
    Added join and left notifications
    Added middle text
    Various CSS tweaks
    Reduced transfer size between client and server
    Created name array on server
    Various method and variable name changes

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
- var express = require('express');
+var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
@@ -9,22 +9,34 @@ app.get('/', function(req, res){
   res.sendfile('index.html');
 });
 
+var clients = {};
 var current_users = 0;
 
 //Whenever someone connects this gets executed
 io.on('connection', function(socket){
-  current_users++;
-  io.sockets.emit('current_users', {current_users: current_users});
+  socket.emit('assign_user_id', {user_id: socket.id});
 
   socket.on('disconnect', function(data) {
-    current_users--;
-    io.sockets.emit('current_users', {current_users: current_users});
+    if (clients[socket.id] !== undefined) {
+      current_users--;
+      io.sockets.emit('remove_user', {current_users: current_users, disconnected_user: clients[socket.id]});
+      delete clients[socket.id];
+    }
+    else {
+      io.sockets.emit('remove_anon', {current_users: current_users});
+    }
+  });
+
+  socket.on('connect_user', function(data) {
+    current_users++;
+    clients[socket.id] = data.user_name;
+    io.sockets.emit('add_user', {current_users: current_users, new_user: data.user_name});
   });
 
   socket.on('sendMessage', function(data) {
-    socket.broadcast.emit('user_message', {sender_name: data.sender_name, sender_id: data.sender_id, message: data.message});
+    var sender_id = socket.id;
+    socket.broadcast.emit('user_message', {sender_name: clients[sender_id], sender_id: sender_id, message: data.message});
   });
-
 
 });
 http.listen(3000, '0.0.0.0', function(){
