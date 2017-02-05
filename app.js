34cabd09a2b9677517b3f51c4f37d917eec976e6
Alunsto <alunsto99@gmail.com>
2017-02-05 17:18:36 -0500

    Misc optimizations

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 var express = require('express');
 var mysql = require('mysql');
 var pool = mysql.createPool({
-  connectionLimit: 100,
-  host: 'localhost',
+  connectionLimit: 1000,
+  host: '104.154.237.176',
   user: 'root',
-  password: '',
+  password: 'V3rbal1rony',
   database: 'chatapp'
 });
 var app = express();
@@ -14,7 +14,7 @@ var io = require('socket.io')(http);
 app.use(express.static('css'));
 app.use(express.static('js'));
 app.get('/', function(req, res){
-  res.sendfile('index.html');
+  res.sendFile(__dirname+'/index.html');
 });
 
 var clients = {};
@@ -25,58 +25,51 @@ io.on('connection', function(socket){
   var address = socket.handshake.address;
   socket.emit('assign_user_id', {user_id: socket.id});
 
-socket.on('disconnect', function(data) {
-  if (clients[socket.id] !== undefined) {
-    current_users--;
-    io.sockets.emit('remove_user', {current_users: current_users, disconnected_user: clients[socket.id]});
-    delete clients[socket.id];
+  socket.on('disconnect', function(data) {
+    if (clients[socket.id] !== undefined) {
+      current_users--;
+      io.sockets.emit('remove_user', {current_users: current_users, disconnected_user: clients[socket.id]});
+      delete clients[socket.id];
 
       var date = new Date();
       var utcDate = date.toUTCString();
-    pool.query("UPDATE users SET end_session = NOW() WHERE user_id = ?", [socket.id], function(err, rows, fields) {
-      if (!err) {
-        console.log("successful update");
-      }
-      else {
-        console.log('update failure');
-      }
-      //  connection.end();
+      pool.getConnection(function(err, connection) {
+        connection.query("UPDATE users SET end_session = NOW() WHERE user_id = ?", [socket.id], function(err, rows, fields) {
+
+        });
+        connection.release();
+      });
+    }
+    else {
+      io.sockets.emit('remove_anon', {current_users: current_users});
+    }
+  });
+
+  socket.on('connect_user', function(data) {
+    var sender_id = socket.id;
+    current_users++;
+    clients[sender_id] = data.user_name;
+    io.sockets.emit('add_user', {current_users: current_users, new_user: data.user_name});
+
+    pool.getConnection(function(err, connection) {
+      connection.query("INSERT INTO users (username, user_id) VALUES (?,?)", [clients[sender_id], sender_id], function(err, rows, fields) {
+
+      });
+      connection.release();
     });
-  }
-  else {
-    io.sockets.emit('remove_anon', {current_users: current_users});
-  }
-});
-
-socket.on('connect_user', function(data) {
-  var sender_id = socket.id;
-  current_users++;
-  clients[sender_id] = data.user_name;
-  io.sockets.emit('add_user', {current_users: current_users, new_user: data.user_name});
-
-  pool.query("INSERT INTO users (username, user_id) VALUES (?,?)", [clients[sender_id], sender_id], function(err, rows, fields) {
-    if (!err) {
-      console.log("successful insert");
-    }
-    else {
-      console.log('insert failure');
-    }
   });
-});
 
-socket.on('sendMessage', function(data) {
-  var sender_id = socket.id;
-  socket.broadcast.emit('user_message', {sender_name: clients[sender_id], sender_id: sender_id, message: data.message});
+  socket.on('sendMessage', function(data) {
+    var sender_id = socket.id;
+    socket.broadcast.emit('user_message', {sender_name: clients[sender_id], sender_id: sender_id, message: data.message});
 
-  pool.query("INSERT INTO messages (message, time_sent, sent_by) VALUES (?,NOW(),?)", [data.message, sender_id], function(err, rows, fields) {
-    if (!err) {
-      console.log("successful insert");
-    }
-    else {
-      console.log(err);
-    }
+    pool.getConnection(function(err, connection) {
+      connection.query("INSERT INTO messages (message, time_sent, sent_by) VALUES (?,NOW(),?)", [data.message, sender_id], function(err, rows, fields) {
+
+      });
+      connection.release();
+    });
   });
-});
 
 });
 http.listen(3000, '0.0.0.0', function(){
